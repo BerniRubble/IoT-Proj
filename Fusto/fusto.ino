f439877e60232ebda7488684b811d2bb83564beb
Manu2497 <75121612+Manu2497@users.noreply.github.com>
2020-12-03 18:25:51 +0100

    Add files via upload

--- Fusto/fusto.ino
+++ Fusto/fusto.ino
@@ -1,14 +1,25 @@
-int level = A0;
-char printBuffer[128];
+//Valori min e max degli intervalli
+#define MIN 150
+#define MAX 210
+
 char id_barrel = 'A';
+
+//Pin
+int level = A0;
 int RED = 2;
 int GREEN = 3;
 int BLUE = 4;
 int BUTTON = 7;
+
+//Var globali codice
 long t = 0;
 long debounce_delay = 200;
+long print_state_delay=5000;
+long t2=0;
 
-int value = 0; // get adc value
+int value = 0; // valore sensore
+
+//Variabili di stato 
 int iFutureState;
 int buttonValue;
 
@@ -48,32 +59,48 @@ void loop()
       digitalWrite(BLUE, LOW);
       break;
     case 2:
+      if((millis()-t2)>print_state_delay)
+      {
+        Serial.print('2');
+        t2=millis();
+      }
+      
       digitalWrite(GREEN, HIGH);
       digitalWrite(RED, LOW);
       digitalWrite(BLUE, LOW);
       break;
     case 3:
       //digitalWrite(GREEN, LOW);
+      //Serial.print('3');
+      if((millis()-t2)>print_state_delay)
+      {
+        Serial.print('3');
+        t2=millis();
+      }
       digitalWrite(RED, HIGH);
       //digitalWrite(BLUE, HIGH);
       setColor(0, 50, 0);
       break;
     case 4:
+      //Serial.print('4');
+      if((millis()-t2)>print_state_delay)
+      {
+        Serial.print('4');
+        t2=millis();
+      }
       digitalWrite(GREEN, LOW);
       digitalWrite(RED, HIGH);
       digitalWrite(BLUE, LOW);
-      delay(1);
+      
       break;
   }
 
 
   iState = iFutureState;
-  //sprintf(printBuffer,"ADC%d level is %d \n",level, value);
-  //sprintf(printState,"State is %d \n", iState);
-  //Serial.print(printBuffer);
-  //Serial.print(printState);
+  
 
 }
+
 
 
 int computeFutureState(int iState, int buttonValue, int value) {
@@ -84,11 +111,11 @@ int computeFutureState(int iState, int buttonValue, int value) {
     //Serial.print(id_barrel);
     t = millis();
   }
-  else if (iState == 1 && value > 210)iFutureState = 2;
-  else if (iState == 1 && value < 210 && value > 100)iFutureState = 3;
-  else if (iState == 1 && value < 100)iFutureState = 4;
-  else if (iState == 2 && value < 210 && value > 100)iFutureState = 3;
-  else if (iState == 3 && value < 100)iFutureState = 4;
+  else if (iState == 1 && value > MAX)iFutureState = 2;
+  else if (iState == 1 && value < MAX && value > MIN)iFutureState = 3;
+  else if (iState == 1 && value < MIN)iFutureState = 4;
+  else if (iState == 2 && value < MAX && value > MIN)iFutureState = 3;
+  else if (iState == 3 && value < MIN)iFutureState = 4;
   else if (iState == 4 && buttonValue == HIGH && (millis() - t) > debounce_delay) {
     iFutureState = 0;
     t = millis();
