f79abb992ac8d72019e581b3c3a8e3791c6559fe
Manu2497 <75121612+Manu2497@users.noreply.github.com>
2021-02-25 15:03:19 +0100

    the end

--- Fusto/fusto.ino
+++ Fusto/fusto.ino
@@ -1,8 +1,8 @@
 //Valori min e max degli intervalli
-#define MIN 150
-#define MAX 210
+#define MIN 140 //150
+#define MAX 170 //210
 
-char id_barrel[3] = "A01";
+char id_barrel[3] = "A01"; //Questo id cambia a seconda della birra e del locale
 
 //Pin
 int level = A0;
@@ -39,7 +39,7 @@ void setup()
 
 void loop()
 {
-  value = analogRead(level); // get adc value
+  value = analogRead(level); 
 
   buttonValue = digitalRead(BUTTON);
 
@@ -52,11 +52,7 @@ void loop()
       digitalWrite(RED, LOW);
       digitalWrite(BLUE, LOW);
       break;
-    case 1:
-      Serial.write('C');
-      Serial.write(id_barrel);
-      Serial.write('\n');
-      
+    case 1:     
       digitalWrite(GREEN, LOW);
       digitalWrite(RED, LOW);
       digitalWrite(BLUE, LOW);
@@ -76,8 +72,6 @@ void loop()
       digitalWrite(BLUE, LOW);
       break;
     case 3:
-      //digitalWrite(GREEN, LOW);
-      //Serial.print('3');
       if((millis()-t2)>print_state_delay)
       {
         Serial.write('S');
@@ -87,11 +81,9 @@ void loop()
         t2=millis();
       }
       digitalWrite(RED, HIGH);
-      //digitalWrite(BLUE, HIGH);
       setColor(0, 50, 0);
       break;
     case 4:
-      //Serial.print('4');
       if((millis()-t2)>print_state_delay)
       {
         Serial.write('S');
@@ -120,7 +112,6 @@ int computeFutureState(int iState, int buttonValue, int value) {
   if (iState == 0 && buttonValue == LOW)iFutureState = 0;
   else if (iState == 0 && buttonValue == HIGH && (millis() - t) > debounce_delay) {
     iFutureState = 1;
-    //Serial.print(id_barrel);
     t = millis();
   }
   else if (iState == 1 && value > MAX)iFutureState = 2;
